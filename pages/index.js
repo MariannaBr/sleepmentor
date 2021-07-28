a16316b4ed9d41cf2614923725c9e96abd3b5de4
marianna <marianna@primeflow.com>
2021-07-28 16:07:23 +0200

    v1 for mobile done

--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,276 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import pic1 from "../public/alexandra-gorn-smuS_jUZa9I-unsplash.jpg";
+import pic2 from "../public/damir-spanic-09znJJdtZFc-unsplash.jpg";
+import ouraStat from "../public/sleepStat.png";
 
 export default function Home() {
   return (
-    <div className="bg-white">
-      <div className="relative overflow-hidden">
-      <div className="pt-10 bg-gray-900 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden">
-        <div className="mx-auto max-w-7xl lg:px-8">
-          <div className="lg:grid lg:grid-cols-2 lg:gap-8">
-            <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
-              <div className="lg:py-24">
-                <a href="#" className="inline-flex items-center text-white bg-black rounded-full p-1 pr-2 sm:text-base lg:text-sm xl:text-base hover:text-gray-200">
-                  <span className="px-3 py-0.5 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-gradient-to-r from-teal-500 to-cyan-600 rounded-full">We're hiring</span>
-                  <span className="ml-4 text-sm">Visit our careers page</span>
-                  <svg className="ml-2 w-5 h-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
-                  </svg>
-                </a>
-                <h1 className="mt-4 text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
-                  <span className="block">A better way to</span>
-                  <span className="pb-3 block bg-clip-text text-transparent bg-gradient-to-r from-teal-200 to-cyan-400 sm:pb-5">ship web apps</span>
-                </h1>
-                <p className="text-base text-gray-300 sm:text-xl lg:text-lg xl:text-xl">
-                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui Lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat.
-                </p>
-                <div className="mt-10 sm:mt-12">
-                  <form action="#" className="sm:max-w-xl sm:mx-auto lg:mx-0">
-                    <div className="sm:flex">
-                      <div className="min-w-0 flex-1">
-                        <label HTMLFor="email" className="sr-only">Email address</label>
-                        <input id="email" type="email" placeholder="Enter your email" className="block w-full px-4 py-3 rounded-md border-0 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"></input>
-                      </div>
-                      <div className="mt-3 sm:mt-0 sm:ml-3">
-                        <button type="submit" className="block w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900">Start free trial</button>
-                      </div>
-                    </div>
-                    <p className="mt-3 text-sm text-gray-300 sm:mt-4">Start your free 14-day trial, no credit card necessary. By providing your email, you agree to our <a href="#" className="font-medium text-white">terms or service</a>.</p>
-                  </form>
-                </div>
-              </div>
-            </div>
-            <div className="mt-12 -mb-16 sm:-mb-48 lg:m-0 lg:relative">
-              <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
-                <img className="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none" src="https://tailwindui.com/img/component-images/cloud-illustration-teal-cyan.svg" alt=""></img>
+    <div className="relative bg-white">
+      <main className="lg:relative">
+        <div className="mx-auto max-w-7xl w-full py-16 text-center lg:py-48 lg:text-left">
+          <div className="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
+            <h1 className="text-4xl tracking-tight font-extrabold text-gray-500 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
+              <span className="block xl:inline">Reclaim your sleep </span>
+              <span className="block text-blue-400 xl:inline">
+                with a sleep mentor
+              </span>
+            </h1>
+          </div>
+        </div>
+        <div className="relative w-full lg:absolute lg:inset-y-20 lg:right-0 lg:w-1/2 lg:h-96">
+          <Image
+            className="absolute inset-0 w-full h-96 object-cover"
+            src={pic2}
+            alt=""
+          />
+        </div>
+      </main>
+
+      <div className="bg-white pt-12">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="max-w-4xl mx-auto text-center">
+            <h2 className="text-3xl font-extrabold text-gray-500 sm:text-4xl">
+              80% of high-achivers are sleep deprived
+            </h2>
+          </div>
+        </div>
+        <div className="mt-10 bg-white">
+          <div className="relative">
+            <div className="relative max-w-7xl mx-auto px-8 sm:px-6 lg:px-8">
+              <div className="max-w-4xl mx-auto">
+                <dl className="rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-3">
+                  <div className="flex flex-col border-b border-gray-100 p-4 text-center sm:border-0 sm:border-r">
+                    <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
+                      IQ Points
+                    </dt>
+                    <dd className="order-1 text-4xl font-extrabold text-red-400">
+                      -15
+                    </dd>
+                  </div>
+                  <div className="flex flex-col border-t border-b border-gray-100 p-4 text-center sm:border-0 sm:border-l sm:border-r">
+                    <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
+                      Testosterone
+                    </dt>
+                    <dd className="order-1 text-4xl font-extrabold text-red-400">
+                      -15%
+                    </dd>
+                  </div>
+                  <div className="flex flex-col border-t border-gray-100 p-4 text-center sm:border-0 sm:border-l">
+                    <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
+                      Ability to burn fat
+                    </dt>
+                    <dd className="order-1 text-4xl font-extrabold text-red-400">
+                      -30%
+                    </dd>
+                  </div>
+                </dl>
               </div>
             </div>
           </div>
         </div>
       </div>
+
+      <div className="pt-16 bg-white overflow-hidden lg:py-24">
+        <div className="relative max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
+          <div className="relative">
+            <h2 className="text-center pb-10 text-3xl leading-8 font-extrabold tracking-tight text-gray-500 sm:text-4xl">
+              A sleep tracker gives you the data...
+            </h2>
+            <h2 className="text-center text-3xl leading-8 font-extrabold tracking-tight text-blue-400 sm:text-4xl">
+              A sleep mentor helps you improve!
+            </h2>
+          </div>
+
+          <div className="relative text-center mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
+            <div className="relative">
+              <h3 className="text-2xl font-extrabold text-gray-500 tracking-tight sm:text-3xl">
+                Our world class mentors are
+              </h3>
+
+              <dl className="mt-6 space-y-2">
+                <div className="relative px-10">
+                  <dt>
+                    <svg
+                      class="absolute h-6 w-6 text-green-500"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M5 13l4 4L19 7"
+                      />
+                    </svg>
+                    <p class="ml-9 text-lg leading-6 font-medium text-gray-500">
+                      Invite team members
+                    </p>
+                  </dt>
+                </div>
+
+                <div className="relative px-10">
+                  <dt>
+                    <svg
+                      class="absolute h-6 w-6 text-green-500"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M5 13l4 4L19 7"
+                      />
+                    </svg>
+                    <p class="ml-9 text-lg leading-6 font-medium text-gray-500">
+                      Invite team members
+                    </p>
+                  </dt>
+                </div>
+
+                <div className="relative px-10">
+                  <dt>
+                    <svg
+                      class="absolute h-6 w-6 text-green-500"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M5 13l4 4L19 7"
+                      />
+                    </svg>
+                    <p class="ml-9 text-lg leading-6 font-medium text-gray-500">
+                      Invite team members
+                    </p>
+                  </dt>
+                </div>
+              </dl>
+            </div>
+
+            <div
+              className="mt-10 relative w-full lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full"
+              aria-hidden="true"
+            >
+              <Image
+                className="absolute mx-auto w-full h-full object-cover"
+                src={ouraStat}
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
       </div>
+
+      <div className="bg-white">
+        <div className="max-w-7xl mx-auto px-4 pt-16 sm:px-6 lg:py-16 lg:px-8">
+          <div className="py-10 px-6 bg-blue-400 rounded-3xl sm:py-16 sm:px-12 lg:p-20 lg:flex lg:items-center">
+            <div className="lg:w-0 lg:flex-1">
+              <h2 className="text-3xl text-center font-extrabold tracking-tight text-white">
+                Reclaim your sleep with SleepMentor
+              </h2>
+            </div>
+            <div className="mt-8 sm:w-full sm:max-w-md lg:mt-0 lg:ml-8 lg:flex-1">
+              <form className="sm:flex">
+                <label for="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email-address"
+                  name="email-address"
+                  type="email"
+                  autocomplete="email"
+                  className="w-full border-white px-5 py-3 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-blue-400 focus:ring-white rounded-md"
+                  placeholder="Enter your email"
+                ></input>
+                <button
+                  type="submit"
+                  className="mt-3 w-full flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-600 hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-700 focus:ring-blue-400 sm:mt-0 sm:ml-3 sm:w-auto sm:flex-shrink-0"
+                >
+                  Notify me
+                </button>
+              </form>
+              <p className="mt-3 text-sm text-gray-500">
+                We care about the protection of your data. Read our{" "}
+                <a href="#" className="text-gray-600 font-medium underline">
+                  Privacy Policy.
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Footer */}
+
+      <footer className="bg-white">
+        <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+          <div className="flex justify-center space-x-6 md:order-2">
+            <a href="#" className="text-gray-400 hover:text-gray-500">
+              <span className="sr-only">Facebook</span>
+              <svg
+                className="h-6 w-6"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </a>
+
+            <a href="#" className="text-gray-400 hover:text-gray-500">
+              <span className="sr-only">Instagram</span>
+              <svg
+                className="h-6 w-6"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </a>
+
+            <a href="#" className="text-gray-400 hover:text-gray-500">
+              <span className="sr-only">Twitter</span>
+              <svg
+                className="h-6 w-6"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+              >
+                <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+              </svg>
+            </a>
+          </div>
+          <div className="mt-8 md:mt-0 md:order-1">
+            <p className="text-center text-base text-gray-400">
+              &copy; 2021 SleepMentor, Inc. All rights reserved.
+            </p>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
+
